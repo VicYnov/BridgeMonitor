59532e9f24f95c921562e750f7fe9eee4a439f4b
victor.garcia@ynov.com <victor.garcia@ynov.com>
2021-04-01 14:10:06 +0200

    Initialisation du répo + connexion à l'Api

---
+++ Models/BridgeMonitor_Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+// Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+public class BridgeMonitor_Model
+{
+    [JsonProperty("boat_name")]
+    public string BoatName { get; set; }
+
+    [JsonProperty("closing_type")]
+    public string ClosingType { get; set; }
+
+    [JsonProperty("closing_date")]
+    public DateTime ClosingDate { get; set; }
+
+    [JsonProperty("reopening_date")]
+    public DateTime ReopeningDate { get; set; }
+}
+
